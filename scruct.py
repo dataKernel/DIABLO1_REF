384cd7ad691526c5980e385a3a2528fca57029bb
Lancelot Saint-Léger <lancelot.saintleger@gmail.com>
2026-08-27 10:51:12 -0700

    creating struct for stats and class with syntax convention

--- scruct.py
+++ scruct.py
@@ -1,1 +1,17 @@
-print("test")
+class_stats = {
+    'statsList': ["health", "mana", "strengh", "dexterity", "vitality", "magic"],
+    'barbStats': {
+        'hp': 300,
+        'mana': 100,
+        'str': 40,
+        'dex': 15,
+        'vit': 250,
+        'mag': 1
+    }
+}
+
+class_class = {
+    'barbarian': {
+        'stats': class_stats['barbStats']
+    }
+}
