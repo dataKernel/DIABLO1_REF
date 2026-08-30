fb786ec4e8e85730a54786436cc66dc47b72d7bd
Lancelot Saint-Léger <lancelot.saintleger@gmail.com>
2026-08-30 14:52:12 +0100

    review structure and key names

--- data.py
+++ data.py
@@ -1,23 +1,14 @@
+#perLvlUp est le nombre de points gagné par niveau (les resistances sont de 1% pour chacune d'entre elles)
 stats = {
-    'statsList': ["Health", "Mana", "Strengh", "Dexterity", "Vitality", "Magic"],
+    'baseStats': ["Health", "Mana", "Strength", "Dexterity", "Vitality", "Magic"],
+    'resistances': ["magic", "fire", "lightning"],
     
     'barb': {
-        'stats': {
-            'hp': 70,
-            'mana': 0,
-            'str': 40,
-            'dex': 20,
-            'vit': 25,
-            'mag': 0
-        },
-        'maxStats': {
-            'hp': 416,
-            'mana': 0,
-            'str': 255,
-            'dex': 55,
-            'vit': 150,
-            'mag': 0
-        }      
+        'stats': {'hp': 70, 'mana': 0, 'str': 40, 'dex': 20 , 'vit': 25, 'mag': 0},
+        'maxStats': {'hp': 416, 'mana': 0, 'str': 255, 'dex': 55,'vit': 150, 'mag': 0},
+        'perLvlUp': {'hp': 2, 'mana': 0,'resistances': 1},
+        'perAttribute': {'vit': 2}
+                
     }
 }
 
@@ -25,6 +16,6 @@ classes = {
     'classes': ["Barbarian", "Warrior", "Rogue", "Sorcerer"],
     
     'barb': {
-        'stats': stats['barb']
+        'stats':stats['barb']
     }
 }
--- struct_version.py
+++ struct_version.py
@@ -1,20 +1,18 @@
-struct_stats = {
-    'baseStats': ["Health", "Mana", "Strengh", "Dexterity", "Magic"]
-}
-
-data_stats = {
-    #pas besoin d'avoir les noms exacts pour ces clefs (usage dev)
+stats = {
+    'baseStats': ["Health", "Mana", "Strengh", "Dexterity", "Magic"],
+    
     'barb': [100, 0, 42, 25, 0],
     'war': [100, 75, 25, 15, 5]
 }
+
 #création des "objets" vide
 barbStats = {}
 warStats = {}
 
-baseStats = struct_stats['baseStats']
+baseStats = stats['baseStats']
 #insertion des datas dans les objets
-for i in range(len(struct_stats['baseStats'])):
-    barbStats[baseStats[i]] = data_stats['barb'][i]
+for i in range(len(stats['baseStats'])):
+    barbStats[baseStats[i]] = stats['barb'][i]
 
 
 print(f"barbStats: {barbStats}")
