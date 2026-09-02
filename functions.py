d5ce4f732d729473a376c8c01de5b3f509b16cfe
Lancelot Saint-Léger <lancelot.saintleger@gmail.com>
2026-09-02 20:36:55 +0100

    add get_res() function with matrix system 2D info and list->tuple convertion for heterogen datas

--- functions.py
+++ functions.py
@@ -2,10 +2,31 @@ import game_data
 
 statsData = game_data.stats
 
+def     get_stats():
+    pass
+
+def     get_base_stats(className=None):
+    pass
+
+def     get_per_lvl_up(className=None):
+    pass
+
+def     get_per_attribute(className=None):
+    pass
+
 def     get_res(className=None):
-    data = statsData[className]
+    data = statsData[className]['res']
     
-    for elem in data:
-        print(f"val: {elem}")
+    matrix = []
+    elems, values = [], [] ## Create 2 lists to store 2D info before tuple conversion
+    for key, val in data.items():
+        elems.append(key)
+        values.append(val)
+    #list convvertion to tuples and add them to the matrix
+    matrix.append(tuple(elems))
+    matrix.append(tuple(values))
     
-get_res('war')
+    return(matrix)
+
+m = get_res('war')
+print(m)
