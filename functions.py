d4aeac47f2f8ec9e09a559c8ef939bc3204051a6
Lancelot Saint-Léger <lancelot.saintleger@gmail.com>
2026-09-08 22:57:13 +0100

    change args in get_baseStat

--- functions.py
+++ functions.py
@@ -16,47 +16,45 @@ def     get_per_attribute(className=None):
 #---------------------- BASE_STATS FUNCTIONS -----------------
 def     get_baseStatsClass(className):
     matrix = []
-    baseStatList, minVal, maxVal = [], ["min"], ["max"] ## Create 3 lists to store 2D info before tuple conversion
+    baseStatsList, minVal, maxVal = [], ["min"], ["max"] # Create 3 lists to store 2D info before tuple conversion
     
     data = statsData[className]['baseStats']
     
     for key, val in data.items():
         # affichage pour recheck les infos: print(f"key:{key} -- data:{val}")
-        baseStatList.append(key)
+        baseStatsList.append(key)
         minVal.append(val['min'])
         maxVal.append(val['max'])   
-    matrix.extend([tuple(baseStatList), tuple(minVal), tuple(maxVal)])
+    matrix.extend([tuple(baseStatsList), tuple(minVal), tuple(maxVal)])
     
     return(matrix)
 
 def     get_baseStats():
-    pass
-
+    matrix = []
+    baseStatsList, valList = list(statsData['war']['baseStats'].keys()), [] #Create 2 lists to store 2D info in matrix before tuple conversion 
+    
+    print(f"baseStatList: {baseStatsList}")
+    
 
 #---------------------- RES FUNCTIONS ------------------------
-def     get_res(): #faudra refactoriser cte func sans le parcours dynamique de clefs sauf si on est sur que on a énormément de type de res à ajouter
+def     get_res():
     matrix = []
-    resList, valList = [], [] ## Create 2 lists to store 2D info before tuple conversion
+    resList, valList = list(statsData['war']['res'].keys()), [] #Create 2 lists to store 2D info before tuple conversion
     
-    check = False
     for keyClass in statsData:
-        if keyClass != "hashMap":
-            #we add the className first to values
-            valList.append(statsData['hashMap'][keyClass])
-            for keyRes, valRes in statsData[keyClass]['res'].items():
-                if not check:
-                    resList.append(keyRes)
-                valList.append(valRes)
-            check = True
-            matrix.append(tuple(valList))
-            valList = [] #we reset the list
+        #we add the className first to values
+        valList.append(commonData.hashmap[keyClass])
+        for valRes in statsData[keyClass]['res'].values():
+            valList.append(valRes)
+        matrix.append(tuple(valList))
+        valList = [] #we reset the list
     matrix.insert(0, tuple(resList))
     
     return(matrix)
 
 def     get_resClass(className):
     matrix = []
-    resList, valList = [], [] ## Create 2 lists to store 2D info before tuple conversion
+    resList, valList = [], [] #Create 2 lists to store 2D info before tuple conversion
     
     data = statsData[className]['res']
     for key, val in data.items():
@@ -68,3 +66,4 @@ def     get_resClass(className):
         
     return(matrix)
 
+print(get_res())
