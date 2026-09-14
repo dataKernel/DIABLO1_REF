f0f9a8dc3cbeed1aebe4f4f6c2f05e166fce91a4
Lancelot Saint-Léger <lancelot.saintleger@gmail.com>
2026-09-14 13:57:27 +0100

    reorganizing common data hashmap for visiblity

--- data/common.py
+++ data/common.py
@@ -1,13 +1,19 @@
 #------------------------ HASHMAP -----------------------
 hashmap = {
-    'war': "Warrior",
-    'rog': "Rogue",
-    'sorc': "Sorcerer",
-    'barb': "Barbarian",
-    'hp': "Health",
-    'mana': "Mana",
-    'str': "Strength",
-    'dex': "Dexterity",
-    'mag': "Magician",
-    'vita': "Vitality"
+    'class':
+    {
+        'war': "Warrior",
+        'rog': "Rogue",
+        'sorc': "Sorcerer",
+        'barb': "Barbarian",
+    },
+    'stats':
+    {    
+        'hp': "Health",
+        'mana': "Mana",
+        'str': "Strength",
+        'dex': "Dexterity",
+        'mag': "Magician",
+        'vita': "Vitality"
+    }
 }
